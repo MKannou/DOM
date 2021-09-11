c93ae89043af9441c3f10dd0cbcd56998c6ba893
Mohamed Kannou <mohamedkannou@gmail.com>
2021-09-11 11:27:13 +0100

    First update

---
+++ js/test.js
@@ -0,0 +1,58 @@
+
+
+
+function Total(qty,ud,total,value){
+ qty=document.getElementById(qty);
+ ud>0?qty.value++:qty.value--;
+ qty.value=Math.max(qty.value,0);
+ document.getElementById(total).value=qty.value*value;
+}
+
+function Totall(qtyl,ud,totall,value){
+ qty=document.getElementById(qtyl);
+ ud>0?qtyl.value++:qtyl.value--;
+ qty.value=Math.max(qtyl.value,0);
+ document.getElementById(totall).value=qtyl.value*value;
+}
+
+function Totalc(qtyc,ud,totalc,value){
+ qty=document.getElementById(qtyc);
+ ud>0?qtyc.value++:qtyc.value--;
+ qty.value=Math.max(qtyc.value,0);
+ document.getElementById(totalc).value=qtyc.value*value;
+}
+
+function Totala(qtya,ud,totala,value){
+ qty=document.getElementById(qtya);
+ ud>0?qtya.value++:qtya.value--;
+ qty.value=Math.max(qtya.value,0);
+ document.getElementById(totala).value=qtya.value*value;
+}
+
+function Totalb(qtyb,ud,totalb,value){
+ qty=document.getElementById(qtyb);
+ ud>0?qtyb.value++:qtyb.value--;
+ qty.value=Math.max(qtyb.value,0);
+ document.getElementById(totalb).value=qtyb.value*value;
+}
+
+function Totalai(qtyai,ud,totalai,value){
+ qty=document.getElementById(qtyai);
+ ud>0?qtyai.value++:qtyai.value--;
+ qty.value=Math.max(qtyai.value,0);
+ document.getElementById(totalai).value=qtyai.value*value;
+}
+
+function Totalca(qtyca,ud,totalca,value){
+ qty=document.getElementById(qtyca);
+ ud>0?qtyca.value++:qtyca.value--;
+ qty.value=Math.max(qtyca.value,0);
+ document.getElementById(totalca).value=qtyca.value*value;
+}
+
+function Totalab(qtyab,ud,totalab,value){
+ qty=document.getElementById(qtyab);
+ ud>0?qtyab.value++:qtyab.value--;
+ qty.value=Math.max(qtyab.value,0);
+ document.getElementById(totalab).value=qtyab.value*value;
+}
